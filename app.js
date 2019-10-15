ad3ed0c25bc58d936034daf819776473b3e38088
jihyyeshin <tlsjh082@gmail.com>
2019-10-15 20:25:26 +0900

    db tag split

--- app.js
+++ app.js
@@ -1,20 +1,28 @@
 'use strict';
 require('dotenv').config({ silent: true });
 
-var express = require('express');
+const express = require('express');
 const request = require('request');
 const botTelegram = require('node-telegram-bot-api');
 const AssistantV1 = require('ibm-watson/assistant/v1');
-
-var app = express();
-var client_id = 'qzqrzckdb6';
-var client_secret = '9XuaJ7GvnX6FCdPHzPwEs7Pg1S7LkU5G0NwC4zfg';
-var fs = require('fs');
-
 const port = 8080;
+const app = express();
+const fs = require('fs');
+
+// for tts, stt
+const client_id = 'qzqrzckdb6';
+const client_secret = '9XuaJ7GvnX6FCdPHzPwEs7Pg1S7LkU5G0NwC4zfg';
+let tagSql = 'select * from tagTB where tag="sad"';
 let context = {}
-let RESULTExample;
-let ResultText, obj;
+//let RESULTExample;
+let ResultText, rseResult, obj;
+let dbTag, watsonRes;
+
+function messageSplitDB(text){// watson에서 온 응답
+	watsonRes=text.split(';'); // watson에서 온 응답을 split으로 나눔
+	dbTag=watsonRes[1]; // tag가 들어가는 곳
+	console.log("dbTag is "+dbTag);
+};
 
 // DB
 const mysql = require('mysql');
@@ -26,10 +34,10 @@ const conn=mysql.createConnection({
 });
 conn.connect();
 
-conn.query('select * from ex', function(err, results, fields){
-	ResultText = results;
+conn.query(tagSql, dbTag, function(err, results, fields){
+	ResultText = results; // 노래의 결과가 저장됨
 	if (err) throw err;
-	console.log('The result is',ResultText[0].title);
+	console.log('DB result is',ResultText);
 });
 
 conn.end();
@@ -54,14 +62,16 @@ telegram.on('message', (msg) => {
 	wAssistant.message({
 		workspace_id: process.env.WORKSPACE_ID,
 		input: {'text': msg.text},
-		context: context
+		//context: context
 	},(err, response) => {
 		if (err)
 			console.log('error:', err);
 		else {
+			messageSplitDB(response.output.text[0]);
 			context = response.context;
-			telegram.sendMessage(chatId, response.output.text[0]+ResultText[0].title); // 답장
-			RESULTExample = response.output.text[0]+ResultText[0].title;
+			messageSplitDB("hello;sad");
+			telegram.sendMessage(chatId,watsonRes[0]+ResultText[0].title); // 답장, 여기에 url
+			//RESULTExample = response.output.text[0]+ResultText[0].title;
 		}
 	});
 });
@@ -84,12 +94,17 @@ telegram.on('message', (msg) => {
 // 	_req.pipe(res); // 브라우저로 출력
 // });
 
+/*
+스피커를 사용했을 때 - 대답해줄 때 왓슨에서 받은 내용을 tts
+스피커에 말을 할 때 해당 내용을 stt
+*/
+
 function tts(){//req, res) {
 	var api_url = 'https://naveropenapi.apigw.ntruss.com/voice/v1/tts';
 	var request = require('request');
 	var options = {
 	  url: api_url,
-	  form: { speaker: 'mijin', speed: '0', text: String("좋은 하루야")}, // result를 여기에 넣어 tts
+	  form: { speaker: 'mijin', speed: '0', text: String("좋은")}, // result를 여기에 넣어 tts
 	  headers: { 'X-NCP-APIGW-API-KEY-ID': client_id, 'X-NCP-APIGW-API-KEY': client_secret },
 	};
 	var writeStream = fs.createWriteStream('./audio/tts1.mp3'); // tts1.mp3 파일 생성
